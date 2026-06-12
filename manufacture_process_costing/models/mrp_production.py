6df931a7b75fa536fa8be54b087a3e9149466afa
Joezervi <programmer@zervi.com>
2026-06-12 10:05:22 +0700

    fixed mrp production costion validate error

--- manufacture_process_costing/models/mrp_production.py
+++ manufacture_process_costing/models/mrp_production.py
@@ -20,6 +20,7 @@
 #
 #############################################################################
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class MrpProduction(models.Model):
