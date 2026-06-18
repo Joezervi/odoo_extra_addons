8bcea4bbfeecc3a3b98e9bc8100e8154163b493c
Joezervi <programmer@zervi.com>
2026-06-18 13:46:58 +0700

    Remove downpayment

--- sale_proforma_invoice/__manifest__.py
+++ sale_proforma_invoice/__manifest__.py
@@ -5,13 +5,12 @@
     "version": "1.0.4",
     "author": "Arun",
     "license": "LGPL-3",
-    "depends": ['sale'],
+    "depends": ["sale"],
     "data": [
-        'views/sale_order_views.xml',
-        'report/report.xml',
+        "report/report.xml",
     ],
     "application": True,
     "installable": True,
     "auto_install": False,
-    'images': ['static/description/2.png'],
+    "images": ["static/description/2.png"],
 }
