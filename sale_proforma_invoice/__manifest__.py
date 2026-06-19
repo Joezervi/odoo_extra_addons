1e74524fc6d0c62c7810121348f0850c9d9c9716
Joezervi <programmer@zervi.com>
2026-06-19 18:02:01 +0700

    Add sale proforma invoice

---
+++ sale_proforma_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Proforma Invoice",
+    "summary": """Sale Proforma Invoice""",
+    "category": "Sales",
+    "version": "1.0.4",
+    "author": "Arun",
+    "license": "LGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+        "report/report.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "images": ["static/description/2.png"],
+}
---
+++ sale_proforma_invoice/models/sale_order.py
@@ -0,0 +1,22 @@
+# models/sale_order.py
+# mohammad change add stage in sale order
+from odoo import api, exceptions, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    down_payment = fields.Monetary(
+        string="Down Payment For Profoma Invoice", currency_field="currency_id"
+    )
+    bank_account = fields.Many2one('res.partner.bank', string="Receive Bank")
+
+    def write(self, vals):
+        if "down_payment" in vals:
+            new_down_payment = vals.get("down_payment", 0.0)
+            for order in self:
+                if new_down_payment > order.amount_total:
+                    raise exceptions.ValidationError(
+                        "Down payment cannot exceed the total order amount."
+                    )
+        return super(SaleOrder, self).write(vals)
